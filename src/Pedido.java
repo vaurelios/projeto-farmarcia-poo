1bfac1d50b6a814dd352a51a95b5f60f70d268b4
Victor A. Santos <victoraur.santos@gmail.com>
2017-06-01 14:21:23 -0300

    add campos das classe Pedido e Produto

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,4 +1,33 @@
+import java.util.Map;
+
+class ProdutoQuantidade {
+    private Produto prod;
+    private int quant;
+
+    public ProdutoQuantidade(Produto prod, int quant)
+    {
+        this.prod = prod;
+        this.quant = quant;
+    }
+
+    public Produto getProduto()
+    {
+        return prod;
+    }
+
+    public int getQuantidade()
+    {
+        return quant;
+    }
+
+    public void setQuantquantidade(int quant)
+    {
+        this.quant = quant;
+    }
+}
 
 public class Pedido {
 
+    private Map<Integer, ProdutoQuantidade> produtos;
+    private Funcionario funcionario;
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,64 @@
 
 public class Produto {
 
+    private int id;
+    private String nome;
+    private String fabricante;
+    private String fornecedor;
+    private String valor;
+
+    public Produto()
+    {
+
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getNome()
+    {
+        return nome;
+    }
+
+    public void setNome(String nome)
+    {
+        this.nome = nome;
+    }
+
+    public String getFabricante()
+    {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante)
+    {
+        this.fabricante = fabricante;
+    }
+
+    public String getFornecedor()
+    {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor)
+    {
+        this.fornecedor = fornecedor;
+    }
+
+    public String getValor()
+    {
+        return valor;
+    }
+
+    public void setValor(String valor)
+    {
+        this.valor = valor;
+    }
 }
